997784d1901f759576afdeeff6b50e0e923c3c5c
Balamiayush <aryanbalami54@gmail.com>
2024-10-15 09:34:30 +0545

    part3

--- config/mongodb-connection.js
+++ config/mongodb-connection.js
@@ -1,6 +1,10 @@
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://aryanbalami54:ecom111@ecomm.hqqpx.mongodb.net/?retryWrites=true&w=majority&appName=ecomm')
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('Could not connect to MongoDB', err))
-    
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://aryanbalami54:ecom111@ecomm.hqqpx.mongodb.net/?retryWrites=true&w=majority&appName=ecomm"
+  )
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Could not connect to MongoDB", err));
+
 module.exports = mongoose.connection;
+ 
--- index.js
+++ index.js
@@ -7,22 +7,29 @@ const multer = require("multer");
 const fs = require("fs");
 const app = express();
 const db = require("./config/mongodb-connection");
-const port = 3000;
+
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-const ownersRoute = require('./routes/ownersRoute')
-const usersRoute = require('./routes/usersRoute')
-const productsRoute  = require('./routes/productsRoute')
 
-app.use('/owners',ownersRoute)
-app.use('/users',usersRoute)
-app.use('/products',productsRoute)
+const ownersRoute = require("./routes/ownersRoute");
+const usersRoute = require("./routes/usersRoute");
+const productsRoute = require("./routes/productsRoute");
 
+const port = 3000;
 
+app.use("/owners", ownersRoute);
+app.use("/users", usersRoute);
+app.use("/products", productsRoute);
 
-app
-  .listen(port)
-  .on("listening", () => console.log(`Server is running on port ${port}`));
+// Start server and log when it's ready
+const server = app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+// If you want to add additional event listeners, use the server object
+server.on("listening", () => {
+  console.log("The server is ready and listening for requests.");
+});
